7d29f275cb58e5d30530ee36f737de0108c08e9d
c_software_aat@outlook.com <c_software_aat@outlook.com>
2022-02-18 12:37:37 +0300

    исправил сообщение

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,6 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");        }
+            Console.WriteLine("Fired");        }
     }
 }
