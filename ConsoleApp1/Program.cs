e456ce9127b125f4573f4b14497a865f593e752f
c_software_aat@outlook.com <c_software_aat@outlook.com>
2022-02-23 14:22:58 +0300

    123

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,6 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Fired");        }
+            Console.WriteLine("Fired 2");        }
     }
 }
